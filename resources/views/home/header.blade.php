d58a75a14d87b88959af0055c19ab79acfae8f0d
UrielElias <urieleliasg@gmail.com>
2018-08-29 15:21:59 -0500

    Desing home view

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,26 @@
+<header role="banner" class="probootstrap-header">
+    <div class="container">
+        <a href="index.html" class="probootstrap-logo">Vintage<span></span></a>
+        
+        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
+        <div class="mobile-menu-overlay"></div>
+
+        <nav role="navigation" class="probootstrap-nav hidden-xs">
+            <ul class="probootstrap-main-nav">
+            <li class="active"><a href="index.html">Inicio</a></li>
+            <li><a href="about.html">Acerca de</a></li>
+            <li><a href="gallery.html">Clientes</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            </ul>
+            <div class="extra-text visible-xs"> 
+            <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
+            <h5>Connect</h5>
+            <ul class="social-buttons">
+                <li><a href="#"><i class="icon-twitter"></i></a></li>
+                <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                <li><a href="#"><i class="icon-instagram2"></i></a></li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ resources/views/home/sliderTop.blade.php
@@ -0,0 +1,19 @@
+<section class="probootstrap-slider flexslider">
+    <div class="probootstrap-text-intro">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+            <div class="probootstrap-slider-text text-center">
+                <h1 class="probootstrap-heading probootstrap-animate mb20" data-animate-effect="fadeIn">100% Organic</h1>
+                <div class="probootstrap-animate probootstrap-sub-wrap mb30" data-animate-effect="fadeIn">A new website template by <a href="https://uicookies.com/">uicookies.com</a> under license <a href="https://uicookies.com/license/">Creative Commons 3.0</a></div>
+                <p class="probootstrap-animate" data-animate-effect="fadeIn"><a href="#" class="btn btn-ghost btn-ghost-white">Get This</a></p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <ul class="slides">
+        <li style="background-image: url(img/slider_1.jpg);"></li>
+        <li style="background-image: url(img/slider_2.jpg);"></li>
+    </ul>
+</section>
